d58bf8ef0298f81bb1571a766e15ae23b6ecd025
Jean-François Lissart <jeanfrancois.lissart@sdworx.com>
2024-12-17 08:56:59 +0100

    Contenu pleine page

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+	const languageLinks = document.querySelectorAll('#language-switcher a');
+	languageLinks.forEach(link => {
+		link.addEventListener('click', function(event) {
+			event.preventDefault();
+			const newUrl = this.getAttribute('href');
+			window.location.href = '../../sites/' + newUrl + '/index.html';
+		});
+	});
+});
