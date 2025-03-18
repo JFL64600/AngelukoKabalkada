8b6565f9120d8937082ab60a6617c5c2f951c53a
Bastien <bmoulia@gmail.com>
2025-03-18 23:41:28 +0100

    footer data from firestore

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,8 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { collection, Firestore, getDocs, query } from '@angular/fire/firestore';
 
 interface FooterSection {
+  id: string;
   title: string;
-  links: { title: string; url: string }[];
+  links: { id: string; title: string; url: string }[];
 }
 
 @Component({
@@ -11,31 +13,34 @@ interface FooterSection {
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
 })
-export class FooterComponent {
-  footerSections = signal<FooterSection[]>([
-    {
-      title: 'About',
-      links: [
-        { title: 'Company', url: '/company' },
-        { title: 'Contact', url: '/contact' },
-        { title: 'Careers', url: '/careers' },
-      ],
-    },
-    {
-      title: 'Services',
-      links: [
-        { title: 'Consulting', url: '/consulting' },
-        { title: 'Development', url: '/development' },
-        { title: 'Design', url: '/design' },
-      ],
-    },
-    {
-      title: 'Legal',
-      links: [
-        { title: 'Privacy', url: '/privacy' },
-        { title: 'Terms', url: '/terms' },
-        { title: 'Cookies', url: '/cookies' },
-      ],
-    },
-  ]);
+export class FooterComponent implements OnInit {
+  #firestore = inject(Firestore);
+
+  footerSections = signal<FooterSection[]>([]);
+
+  async ngOnInit() {
+    const footerSections: FooterSection[] = [];
+    const querySnapshot = await getDocs(collection(this.#firestore, 'footer'));
+    querySnapshot.forEach((doc) => {
+      const footerSection: FooterSection = {
+        id: doc.id,
+        title: doc.data()['title'],
+        links: [],
+      };
+      footerSections.push(footerSection);
+    });
+    for await (const footerSection of footerSections) {
+      const querySnapshot = await getDocs(
+        collection(this.#firestore, 'footer', footerSection.id, 'links')
+      );
+      querySnapshot.forEach((doc) => {
+        footerSection.links.push({
+          id: doc.id,
+          title: doc.data()['title'],
+          url: doc.data()['url'],
+        });
+      });
+    }
+    this.footerSections.set(footerSections);
+  }
 }
