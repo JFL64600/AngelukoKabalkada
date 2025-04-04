8cb76822d0fb69ab835fcc5b6036637c4acff870
Bastien <bmoulia@gmail.com>
2025-04-04 13:29:34 +0200

    Add ngx-owl-carousel-o for enhanced header carousel functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { BodyComponent } from './body/body.component';
-import { FooterComponent } from './footer/footer.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
