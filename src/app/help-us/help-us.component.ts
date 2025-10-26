12f9756369d880bb909706734a5cd4de75b02535
Bastien <bmoulia@gmail.com>
2025-10-26 13:33:17 +0100

    feat: update Help Us section to use markdown for individuals and businesses information

--- src/app/help-us/help-us.component.ts
+++ src/app/help-us/help-us.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { TranslocoDirective } from '@jsverse/transloco';
+import { MarkdownComponent } from 'ngx-markdown';
 
 @Component({
   selector: 'anka-help-us',
-  imports: [TranslocoDirective, MatButton],
+  imports: [TranslocoDirective, MatButton, MarkdownComponent],
   templateUrl: './help-us.component.html',
   styleUrl: './help-us.component.css',
 })
