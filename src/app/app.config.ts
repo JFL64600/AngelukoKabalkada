178f5923d90978480232a4317b4e1eda045e6264
Bastien <bmoulia@gmail.com>
2026-01-13 23:24:16 +0100

    refactor: replace router outlet with main component and remove routing configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,8 @@ import {
   provideZonelessChangeDetection,
   isDevMode,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
-import { routes } from './app.routes';
 import { provideMarkdown } from 'ngx-markdown';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { TranslocoHttpLoader } from './transloco-loader';
@@ -15,7 +13,6 @@ import { provideTransloco } from '@jsverse/transloco';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
-    provideRouter(routes),
     provideAnimations(),
     provideMarkdown(),
     provideHttpClient(withFetch()),
